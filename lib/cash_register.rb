0f8b36f88c8161c7452cd8ffcd0bcd8d2aae56c4
Tony Baker <this.tonybaker@gmail.com>
2019-04-06 00:06:55 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,10 @@
 class CashRegister
-  attr_accessor :employee_discount, :total, :title, :quantity, :price, :item_array
+  attr_accessor :discount, :total, :title, :quantity, :price, :item_array
 
   def initialize(discount = 0)
-    @employee_discount = discount
+    @discount = discount
     @total = 0
     @item_array = []
-  end
-
-  def discount
-    @employee_discount
   end
 
   def add_item(title, price, quantity = 1)
@@ -22,10 +18,10 @@ class CashRegister
   end
 
   def apply_discount
-    if @employee_discount == 0
+    if @discount == 0
       return "There is no discount to apply."
     else
-      discount = @employee_discount.to_f / 100
+      discount = @discount.to_f / 100
       amount_off = @total * discount
       @total = @total - amount_off
       "After the discount, the total comes to $#{@total.to_i}."
